3615aafb534f6580723639bf7fe8df63a07ba94f
Emil MAMMADZADA <emammadz@e1r8p14.42.fr>
2017-03-23 16:15:34 +0100

    grace works fine now, added file creation

--- Grace.c
+++ Grace.c
@@ -5,5 +5,5 @@ FILE *file = fopen("Grace_kid.c", "w"); \
 /*\
 	Colleen sucks!\
 */\
-char*a="#include <stdio.h>%c#define TEST 1%c#define FT() int main(){ %c%cFILE *file = fopen(%cGrace_lid.c%c, %cw%c); %c%c/*%c%c%cColleen sucks!%c%c*/%c%cchar*a=%c%s%c;printf(a,10,10,92,10,34,34,34,34,92,10,92,10,9,92,10,92,10,34,a,34,10);} fclose(FILE);%cFT();";printf(a,10,10,92,10,34,34,34,34,92,10,92,10,9,92,10,92,10,34,a,34,10);} fclose(FILE);
+char*a="#include <stdio.h>%c#define TEST 1%c#define FT() int main(){ %c%cFILE *file = fopen(%cGrace_kid.c%c, %cw%c); %c%c/*%c%c%cColleen sucks!%c%c*/%c%cchar*a=%c%s%c;fprintf(file,a,10,10,92,10,34,34,34,34,92,10,92,10,9,92,10,92,10,34,a,34,10); fclose(file);}%cFT();";fprintf(file,a,10,10,92,10,34,34,34,34,92,10,92,10,9,92,10,92,10,34,a,34,10); fclose(file);}
 FT();
